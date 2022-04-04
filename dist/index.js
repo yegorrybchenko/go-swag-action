8daf7f724729f3fecd048b64e9b0d2cbcaa99dc5
Yegor Rybchenko <yegorrybchenko@gmail.com>
2022-04-05 02:25:28 +0400

    print count

--- dist/index.js
+++ dist/index.js
@@ -100,12 +100,17 @@ function compareGoFiles(generatedFilePath) {
             return;
         }
         for (const change of changes) {
-            const color = change.added
-                ? '\u001b[32m'
-                : change.removed
-                    ? '\u001b[31m'
-                    : '\u001b[90m';
-            core.info(`${color}${change.value}`);
+            if (change.count) {
+                core.debug(change.count.toString());
+            }
+            if (change.added) {
+                const greenColor = '\u001b[32m';
+                core.info(`${greenColor}${change.value}`);
+            }
+            else if (change.removed) {
+                const redColor = '\u001b[31m';
+                core.info(`${redColor}${change.value}`);
+            }
         }
         throw new Error(`Go files are not equal`);
     });
--- src/compare-go-files.ts
+++ src/compare-go-files.ts
@@ -21,13 +21,17 @@ export async function compareGoFiles(generatedFilePath: string): Promise<void> {
   }
 
   for (const change of changes) {
-    const color = change.added
-      ? '\u001b[32m'
-      : change.removed
-      ? '\u001b[31m'
-      : '\u001b[90m'
+    if (change.count) {
+      core.debug(change.count.toString())
+    }
 
-    core.info(`${color}${change.value}`)
+    if (change.added) {
+      const greenColor = '\u001b[32m'
+      core.info(`${greenColor}${change.value}`)
+    } else if (change.removed) {
+      const redColor = '\u001b[31m'
+      core.info(`${redColor}${change.value}`)
+    }
   }
 
   throw new Error(`Go files are not equal`)
