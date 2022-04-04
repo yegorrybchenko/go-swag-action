bc4f027712dc4ffdc33c98cd3b3f9037d0eb0688
Yegor Rybchenko <yegorrybchenko@gmail.com>
2022-04-04 22:52:58 +0400

    fix extract function

--- src/extract.ts
+++ src/extract.ts
@@ -1,19 +1,30 @@
 import * as core from '@actions/core'
 import * as tc from '@actions/tool-cache'
+import fs from 'fs'
+import {ok} from 'assert'
+import path from 'path'
 
 const downloadPath = 'https://github.com/swaggo/swag/releases/download/'
 
 export async function extractTool(version: string): Promise<string> {
   const fullDownloadPath = getFullDownloadPath(version)
-  core.info(`Installing tool ${version}...`)
+  core.debug(`Download path is ${fullDownloadPath}`)
+
+  core.info(`Installing swag tool ${version}...`)
   const toolPathZip = await tc.downloadTool(fullDownloadPath)
 
-  const homeBinPath = `${process.env.HOME}/bin`
-  const toolPath = await tc.extractTar(toolPathZip, homeBinPath)
+  const toolPathDirectory = await tc.extractTar(toolPathZip)
 
-  core.addPath(toolPath)
+  const swagToolPath = path.join(toolPathDirectory, 'swag')
 
-  return toolPath
+  const homeBinPath = `${_getHOMEDirectory()}/bin`
+  ok(homeBinPath, 'Expected HOME/bin to be defined')
+  const newSwagToolPath = path.join(homeBinPath, 'swag')
+  core.debug(`New swag tool path is ${newSwagToolPath}`)
+
+  fs.copyFileSync(swagToolPath, newSwagToolPath)
+
+  return newSwagToolPath
 }
 
 function getFullDownloadPath(version: string): string {
@@ -27,4 +38,10 @@ function getFullDownloadPath(version: string): string {
   }
 
   return `${downloadPath}v${version}/swag_${version}_${platform}_x86_64.tar.gz`
+}
+
+function _getHOMEDirectory(): string {
+  const homeDirectory = process.env.HOME || ''
+  ok(homeDirectory, 'Expected HOME to be defined')
+  return homeDirectory
 }
